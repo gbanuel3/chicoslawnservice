e7375c7a8e8d25d72c3f01230cfb99b276f4f563
Gil Banuelos <gbanuel3@gmail.com>
2024-02-26 08:02:22 -0600

    done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ function Index() {
       Welcome to my very basic Website
       </Text>
       <Text size="18px">
-      This is the place where I share my projects, thoughts, and ideas.
+      This is where I share my projects, thoughts, and ideas.
       </Text>
     </VStack>
     </>
