c23f7ec43d63600b6bbd4a3eda3b886fb06e3c5e
Gil Banuelos <gbanuel3@gmail.com>
2024-02-26 05:07:40 -0600

    update to about text

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -6,7 +6,7 @@ function About() {
       <h1>About Us</h1>
       <p>
         This is the About page. Here you can add information about your project, your
-        team, or any other details you wish to share. This is automatically updated on commit to main!
+        team, or any other details you wish to share!
       </p>
     </div>
   )
