afa394a79434bd3ffd37caab0d2175c7480184d0
Gil Banuelos <gbanuel3@gmail.com>
2024-03-14 02:37:30 -0500

    projects section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { VStack } from '@chakra-ui/react'
 import Slideshow from '@/components/slideshow'
 import Services from '@/components/services'
+import Projects from '@/components/projects'
 
 function Index() {
   return (
@@ -9,6 +10,7 @@ function Index() {
       <VStack spacing={0}>
         <Slideshow></Slideshow>
         <Services></Services>
+        <Projects></Projects>
       </VStack>
     </>
   )
--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -5,6 +5,19 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
   }
+  .swiper-button-next, .swiper-button-prev {
+    color: white !important; /* Set arrow icon color */
+    opacity: 1 !important; /* Make sure it's fully visible */
+}
+
+/* Optionally, adjust the position slightly if needed */
+.swiper-button-next {
+    right: 10px !important;
+}
+
+.swiper-button-prev {
+    left: 10px !important;
+}
 `
 
 export default GlobalStyle
