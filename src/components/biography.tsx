7d5ddb8a17f91e2a2dea501049757f1cd32f8788
Gil Banuelos <gbanuel3@gmail.com>
2024-03-14 05:00:52 -0500

    reviews done

--- src/components/biography.tsx
+++ src/components/biography.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import { Box, Flex, Image, Text, useBreakpointValue } from '@chakra-ui/react';
+import React from 'react'
+import { Box, Flex, Image, Text, useBreakpointValue } from '@chakra-ui/react'
 
 function Biography() {
   // Adjusting image size dynamically
-  const imageSize = useBreakpointValue({ base: '150px', md: '225px', lg: '300px' });
+  const imageSize = useBreakpointValue({ base: '150px', md: '225px', lg: '300px' })
   // Adjusting text size dynamically
-  const textSize = useBreakpointValue({ base: 'sm', md: 'md', lg: 'lg' });
+  const textSize = useBreakpointValue({ base: 'sm', md: 'md', lg: 'lg' })
   // Adjusting the maximum width of the text container dynamically
-  const textMaxWidth = useBreakpointValue({ base: '100%', md: '600px', lg: '800px' });
+  const textMaxWidth = useBreakpointValue({ base: '100%', md: '600px', lg: '800px' })
 
   return (
     <Box p={5} bg="black" color="white" width={'100%'}>
@@ -32,7 +32,7 @@ function Biography() {
         <Text
           fontSize={textSize} // Use the dynamic text size
           maxWidth={textMaxWidth} // Use the dynamic max width for the text container
-          textAlign={{ base: 'center', md: 'center' }}
+          textAlign={{ base: 'center', md: 'left' }}
         >
           For 29 years, our landscaping business has been transforming the Chicagoland
           area, bringing life and beauty to outdoor spaces. Specializing in custom
@@ -43,7 +43,7 @@ function Biography() {
         </Text>
       </Flex>
     </Box>
-  );
+  )
 }
 
-export default Biography;
+export default Biography
