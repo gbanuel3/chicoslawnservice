b8d15de9f76dad24f9c0be6f0ffc4e74b79585b4
Gil Banuelos <gbanuel3@gmail.com>
2024-03-14 18:15:23 -0500

    prevoew

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -30,7 +30,24 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head></Head>
+        <Head>
+        <title>Chico's Lawn Service</title>
+          <meta name="title" content="Chico's Lawn Service" />
+          <meta name="description" content="Operating in the Chicagoland area - trusted for over 25 years!" />
+
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://chicoslawnservice.com" /> 
+          <meta property="og:title" content="Chico's Lawn Service" />
+          <meta property="og:description" content="Operating in the Chicagoland area - trusted for over 25 years!" />
+          <meta property="og:image" content="https://chicoslawnservice.com/logo.webp" /> 
+
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta property="twitter:url" content="https://chicoslawnservice.com" /> 
+          <meta property="twitter:title" content="Chico's Lawn Service" />
+          <meta property="twitter:description" content="Operating in the Chicagoland area - trusted for over 25 years!" />
+          <meta property="twitter:image" content="https://chicoslawnservice.com/logo.webp" /> 
+
+        </Head>
         <body>
           <Main />
           <NextScript />
